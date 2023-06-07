fdd6b8d388bd55b0eb05a45cd3161d3dc236b4ed
Madhubhashana99 <104136158+Madhubhashana99@users.noreply.github.com>
2023-06-07 10:50:40 +0530

    Lazy singleton done

--- untitled/src/SingletonDemo.java
+++ untitled/src/SingletonDemo.java
@@ -1,7 +1,11 @@
 public class SingletonDemo {
     public static void main(String[] args) {
+        //Eager singleton
         EagerSingleton eager = EagerSingleton.getInstance();
         eager.showMessage();
 
+        //Lazy Singleton
+        LazySingleton lazy = LazySingleton.getInstance();
+        lazy.showMessage();
     }
 }
